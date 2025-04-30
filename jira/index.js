975fe700120eb4462bdfc7abbc3c5d3e8af346bc
Fiona Runge <fiona.runge@gridfuse.com>
2025-04-30 11:48:27 +0200

    keep nuking

--- jira/index.js
+++ jira/index.js
@@ -1,9 +1,29 @@
-window.setTimeout(() => {
-  console.log("Hello Fiona. Let us burn down some AI 🔥");
+console.log("Hello Fiona. Let us burn down some AI 🔥");
+
+window.setInterval(() => {
   document.querySelectorAll(".acronym-highlight").forEach((node) => {
     node.removeAttribute("style");
   });
+
   document
     .querySelectorAll('[aria-label="AI"]')
     .forEach((node) => node.remove());
-}, 1_000);
+
+  document
+    .querySelectorAll('[aria-label="Quick comments"]')
+    .forEach((node) => node.remove());
+
+  document
+    .querySelectorAll(
+      '[data-testid="issue-smart-request-summary.ui.ai-container"]'
+    )
+    .forEach((node) => node.remove());
+
+  document
+    .querySelectorAll('[data-testid="atlassian-intelligence-toolbar-button"]')
+    .forEach((node) => node.remove());
+
+  document
+    .querySelectorAll('[data-testid="contextual-pulse-none"]')
+    .forEach((node) => node.remove());
+}, 500);
